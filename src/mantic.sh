08d74a7835570d223334eb0f5584d8e0603258cf
sharpordie <72373746+sharpordie@users.noreply.github.com>
2023-11-06 07:03:31 +0100

    small update

--- src/mantic.sh
+++ src/mantic.sh
@@ -15,6 +15,193 @@ update_chromium() {
 	sudo add-apt-repository -y ppa:xtradeb/apps
 	sudo apt update && sudo apt install -y ungoogled-chromium
 
+    # Change desktop
+	local desktop="/usr/share/applications/ungoogled-chromium.desktop"
+	sudo sed -i "s/Name=.*/Name=Chromium/" "$desktop"
+
+    # Change environment
+	local configs="$HOME/.bashrc"
+	if ! grep -q "CHROME_EXECUTABLE" "$configs" 2>/dev/null; then
+		[[ -s "$configs" ]] || touch "$configs"
+		[[ -z $(tail -1 "$configs") ]] || echo "" >>"$configs"
+		echo 'export CHROME_EXECUTABLE="/usr/bin/ungoogled-chromium"' >>"$configs"
+		export CHROME_EXECUTABLE="/usr/bin/ungoogled-chromium"
+	fi
+
+    # Finish installation
+	if [[ "$present" == "false" ]]; then
+		# Launch chromium
+		sleep 1 && (sudo ydotoold &) &>/dev/null
+		sleep 1 && (ungoogled-chromium --lang=en --start-maximized &) &>/dev/null
+		sleep 4 && sudo ydotool key 125:1 103:1 103:0 125:0
+
+		# Change deposit
+		mkdir -p "$deposit"
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://settings/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "before downloading" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 3); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 56:1 15:1 15:0 56:0 && sleep 1 && sudo ydotool key 56:1 15:1 15:0 56:0
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0 && sleep 1 && sudo ydotool type "$deposit" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 15:1 15:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change search engine
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://settings/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "search engines" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 3); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "duckduckgo" && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change custom-ntp flag
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "custom-ntp" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 5); do sleep 0.5 && sudo ydotool key 15:1 15:0; done
+		sleep 1 && sudo ydotool key 29:1 30:1 30:0 29:0 && sleep 1 && sudo ydotool type "$startup"
+		sleep 1 && for i in $(seq 1 2); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change disable-sharing-hub flag
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "disable-sharing-hub" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change extension-mime-request-handling flag
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "extension-mime-request-handling" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 2); do sleep 0.5 && sudo ydotool key 108:1 108:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change hide-sidepanel-button flag
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "hide-sidepanel-button" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change remove-tabsearch-button flag
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "remove-tabsearch-button" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Change show-avatar-button flag
+		sleep 1 && sudo ydotool key 29:1 38:1 38:0 29:0
+		sleep 1 && sudo ydotool type "chrome://flags/" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && sudo ydotool type "show-avatar-button" && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 6); do sleep 0.5 && sudo ydotool key 15:1 15:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+		sleep 1 && for i in $(seq 1 3); do sleep 0.5 && sudo ydotool key 108:1 108:0; done && sleep 1 && sudo ydotool key 28:1 28:0
+
+		# Toggle bookmark bar
+		sleep 4 && sudo ydotool key 29:1 42:1 48:1 48:0 42:0 29:0
+
+		# Finish chromium
+		sleep 4 && sudo ydotool key 56:1 62:1 62:0 56:0
+
+		# Update chromium-web-store extension
+		local adjunct="NeverDecaf/chromium-web-store"
+		local address="https://api.github.com/repos/$adjunct/releases/latest"
+		local version=$(curl -LA "mozilla/5.0" "$address" | jq -r ".tag_name" | tr -d "v")
+		update_chromium_extension "https://github.com/$adjunct/releases/download/v$version/Chromium.Web.Store.crx"
+
+		# Update some extensions
+		update_chromium_extension "bcjindcccaagfpapjjmafapmmgkkhgoa" # json-formatter
+		update_chromium_extension "ibplnjkanclpjokhdolnendpplpjiace" # simple-translate
+		update_chromium_extension "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsorblock-for-youtube
+		update_chromium_extension "cjpalhdlnbpafiamejdnhcphjbkeiagm" # ublock-origin
+	fi
+
+	# Update bypass-paywalls-chrome-clean extension
+	local address="https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean"
+	local address="$address/-/archive/master/bypass-paywalls-chrome-clean-master.zip"
+	update_chromium_extension "$address"
+
+}
+
+update_chromium_extension() {
+
+	# Handle parameters
+	local payload=${1}
+
+	# Update dependencies
+	update_ydotool || return 1
+	sudo apt install -y curl
+
+	# Update extension
+	if [[ -x $(command -v chromium) ]]; then
+		if [[ "$payload" == http* ]]; then
+			local address="$payload"
+			local package="$(mktemp -d)/$(basename "$address")"
+		else
+			local version=$(ungoogled-chromium --product-version)
+			local address="https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3"
+			local address="${address}&prodversion=${version}&x=id%3D${payload}%26installsource%3Dondemand%26uc"
+			local package="$(mktemp -d)/$payload.crx"
+		fi
+		curl -LA "mozilla/5.0" "$address" -o "$package" || return 1
+		if [[ "$package" == *.zip ]]; then
+			local deposit="$HOME/.config/chromium/Unpacked/$(echo "$payload" | cut -d / -f5)"
+			local present=$([[ -d "$deposit" ]] && echo "true" || echo "false")
+			mkdir -p "$deposit"
+			bsdtar -zxf "$package" -C "$deposit" --strip-components=1 || return 1
+			[[ "$present" == "true" ]] && return 0
+			sleep 2 && (sudo ydotoold &) &>/dev/null
+			sleep 2 && (ungoogled-chromium --lang=en --start-maximized &) &>/dev/null
+			sleep 4 && sudo ydotool key 29:1 38:1 38:0 29:0
+			sleep 2 && sudo ydotool type "chrome://extensions/" && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool key 15:1 15:0 && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool key 15:1 15:0 && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool type "$deposit" && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool key 15:1 15:0 && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 4 && sudo ydotool key 56:1 62:1 62:0 56:0
+			sleep 2 && (ungoogled-chromium --lang=en --start-maximized &) &>/dev/null
+			sleep 4 && sudo ydotool key 29:1 38:1 38:0 29:0
+			sleep 2 && sudo ydotool type "chrome://extensions/" && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool key 15:1 15:0 && sleep 2 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool key 56:1 62:1 62:0 56:0
+		else
+			sleep 1 && (sudo ydotoold &) &>/dev/null
+			sleep 1 && (ungoogled-chromium --lang=en --start-maximized "$package" &) &>/dev/null
+			sleep 4 && sudo ydotool key 108:1 108:0 && sleep 1 && sudo ydotool key 28:1 28:0
+			sleep 2 && sudo ydotool key 56:1 62:1 62:0 56:0
+		fi
+	fi
+
+}
+
+update_system() {
+
+	# Handle parameters
+	local country=${1:-Europe/Brussels}
+	local machine=${2:-ubuhogen}
+
+	# Change hostname
+	hostnamectl hostname "$machine"
+
+	# Change timezone
+	sudo unlink "/etc/localtime"
+	sudo ln -s "/usr/share/zoneinfo/$country" "/etc/localtime"
+
+	# Change network
+	local configs="/etc/NetworkManager/conf.d/default-wifi-powersave-on.conf"
+	sudo sed -i "s/wifi.powersave =.*/wifi.powersave = 2/" "$configs"
+	sudo systemctl disable NetworkManager-wait-online.service
+
+	# Update firmware
+	sudo fwupdmgr get-devices -y
+	sudo fwupdmgr refresh --force
+	sudo fwupdmgr get-updates -y
+	sudo fwupdmgr refresh --force
+	sudo fwupdmgr update -y
+
+	# Update system
+	sudo apt update -y && sudo apt upgrade -y
+	sudo apt full-upgrade -y && sudo apt autoremove -y
+
 }
 
 update_ydotool() {
@@ -54,7 +241,7 @@ main() {
 		╚██████╔╝██████╦╝╚██████╔╝██║░░██║╚█████╔╝╚██████╔╝███████╗██║░╚███║
 		░╚═════╝░╚═════╝░░╚═════╝░╚═╝░░╚═╝░╚════╝░░╚═════╝░╚══════╝╚═╝░░╚══╝
 	EOD
-	printf "\n\033[92m%s\033[00m\n\n" "$welcome"
+	clear && printf "\n\033[92m%s\033[00m\n\n" "$welcome"
 
 	# Remove timeouts
 	echo "Defaults timestamp_timeout=-1" | sudo tee "/etc/sudoers.d/disable_timeout" &>/dev/null
@@ -67,7 +254,7 @@ main() {
 	# Handle members
 	local members=(
 		"update_appearance"
-		# "update_system"
+		"update_system"
 		"update_chromium"
 		# "update_git 'main' 'sharpordie' '72373746+sharpordie@users.noreply.github.com'"
 		# "update_vscode"
